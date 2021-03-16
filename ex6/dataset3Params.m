a0a5effcceb31496efb3b02eae57809f32ed0c2a
Kerry Liu <kerryliukerui@gmail.com>
2021-03-16 17:56:01 -0400

    Completed exercise for week7 (ex6)

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -24,10 +24,25 @@ sigma = 0.3;
 %
 
 
+results = zeros(64, 3); 
+errorRow = 0; 
 
+for C_temp = [0.01 0.03 0.1 0.3 1, 3, 10 30]
+    for sigma_temp = [0.01 0.03 0.1 0.3 1, 3, 10 30]
+        errorRow = errorRow + 1; 
+        model = svmTrain(X, y, C_temp, @(x1, x2)gaussianKernel(x1, x2, sigma_temp));
+        predictions = svmPredict(model, Xval); 
+        prediction_error = mean(double(predictions ~= yval)); 
+        
+        results(errorRow, :) = [C_temp, sigma_temp, prediction_error]; 
+        
+    end
+end
 
+sorted_results = sortrows(results, 3); % sort matrix by column#3 (error) ascending
 
-
+C = sorted_results(1, 1); 
+sigma = sorted_results(1, 2); 
 
 % =========================================================================
 
